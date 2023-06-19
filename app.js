cb48c9132eb58d8b99a9f526164308746458b6e2
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-19 18:27:17 +0300

    sort

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('App.js');
+
+const colors = ['red', 'green', 'blue', 'apple'];
+
+console.log('colors ===', colors);
+
+// paprastas isrikiavimas
+colors.sort();
+
+console.log('colors after sort ===', colors);
+
+colors.reverse();
+
+console.log('colors after reverse ===', colors);
