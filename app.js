f80bb523d283bb9d520c6e905d5c8f4a83546b6e
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-19 18:45:33 +0300

    sort

--- app.js
+++ app.js
@@ -13,3 +13,36 @@ console.log('colors after sort ===', colors);
 colors.reverse();
 
 console.log('colors after reverse ===', colors);
+
+// skaitinis rikiavimas
+
+const nums = [7, 1, 0, -10, 50, -5];
+
+console.log('nums', nums);
+
+nums.sort();
+
+console.log('nums.sort()', nums);
+
+// sorting nums with function
+//sort
+nums.sort((a, b) => a - b);
+
+console.log('nums.sort((a, b) => a - b);', nums);
+
+// reverse
+nums.sort((a, b) => b - a);
+
+console.log('nums.sort((a, b) => b - a);', nums);
+
+//
+
+nums.sort((num1, num2) => {
+  // turi grazinti neigiama, teigiama arba 0
+  if (num1 > num2) return -1;
+
+  if (num1 < num2) return 1;
+
+  return 0;
+});
+console.log('nums.sort(expanded)', nums);
